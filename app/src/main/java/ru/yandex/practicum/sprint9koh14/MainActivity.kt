ae0d5f8b428478fa2a79d5e458342771b7314d40
Aleksei Anisimov <suxelshock@gmail.com>
2024-02-14 20:14:32 +0300

    Настройка стилей и тем

--- app/src/main/java/ru/yandex/practicum/sprint9koh14/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint9koh14/MainActivity.kt
@@ -1,11 +1,28 @@
 package ru.yandex.practicum.sprint9koh14
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        var themeResId = R.style.Theme_Sprint9koh14
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(themeResId)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<MaterialButton>(R.id.theme1).setOnClickListener {
+            themeResId = R.style.Theme_Sprint9koh14
+            recreate()
+        }
+        findViewById<MaterialButton>(R.id.theme2).setOnClickListener {
+            themeResId = R.style.Theme_Sprint9koh14_Alternative
+            recreate()
+        }
     }
 }
